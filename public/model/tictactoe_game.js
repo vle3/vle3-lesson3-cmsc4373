8a729d944ff4099db5daa0e99237284fdcab6514
Vincent Le <vle3@uco.edu>
2023-02-10 11:45:34 -0600

    check winner methods

---
+++ public/model/tictactoe_game.js
@@ -0,0 +1,66 @@
+export const marking = {
+    X: 'X',
+    O: 'O',
+    U: 'U',
+};
+export class TicTacToeGame {
+    constructor() {
+        this.turn = marking.X; // X plays first 
+        this.moves = 0;
+        this.winner = null; // null (playing), marking.O/X U: draw
+        this.board = [];
+        for (let i = 0; i < 9; i++) {
+            this.board.push(marking.U);
+        }
+
+        this.status = 'Ready to Play';
+    }
+
+    toggleTurn() {
+        this.turn = this.turn == marking.X ? marking.O : marking.X;
+    }
+
+    checkRow(n) {
+        if (this.board[n * 3] != marking.U
+            && this.board[n * 3] == this.board[n * 3 + 1]
+            && this.board[n * 3] == this.board[n * 3 + 2]
+        ) {
+            return this.board[n * 3];
+        } else {
+            return null;
+        }
+    }
+
+    checkCol(n) {
+        if (this.board[n] != marking.U
+            && this.board[n] == this.board[n + 3]
+            && this.board[n] == this.board[n + 6]
+        ) {
+            return this.board[n];
+        } else {
+            return null;
+        }
+    }
+
+    checkDiag1() {
+        if (this.board[2] != marking.U
+            && this.board[2] == this.board[4]
+            && this.board[2] == this.board[6]
+        ) {
+            return this.board[2];
+        } else {
+            return null;
+        }
+    }
+
+    checkDiag2(){
+        if (this.board[0] != marking.U
+            && this.board[0] == this.board[4]
+            && this.board[0] == this.board[8]
+        ) {
+            return this.board[0];
+        } else {
+            return null;
+        }
+    }
+}
--- public/viewpage/tictactoe_page.js
+++ public/viewpage/tictactoe_page.js
@@ -2,6 +2,7 @@ import * as Elements from './elements.js';
 import { routePath } from '../controller/route.js';
 import { currentUser } from '../controller/firebase_auth.js';
 import { unauthorizedAccess } from './unauthorized_access_message.js';
+import { TicTacToeGame, marking,  } from '../model/tictactoe_game.js';
 
 export function addEventListeners() {
     Elements.menus.tictactoe.addEventListener('click', () => {
@@ -11,11 +12,74 @@ export function addEventListeners() {
 
 }
 
-export function tictactoe_page() {
+let gameModel;
+
+let screen = {
+    turn: null,
+    moves: null,
+    buttons: null,
+    images: null,
+};
+
+const imageSource = {
+    X: '/images/X.png',
+    O: '/images/O.png',
+    U: '/images/U.png'
+};
+
+export async function tictactoe_page() {
     if (!currentUser) {
         Elements.root.innerHTML = unauthorizedAccess();
         return;
     }
-    let html = 'TicTacToe Game page';
+
+    gameModel = new TicTacToeGame();
+
+    let html;
+    const response = await fetch('/viewpage/templates/tictactoe_page.html', { cache: 'no-store' });
+    html = await response.text();
     Elements.root.innerHTML = html;
+
+    getScreenElements();
+    addGameEvents();
+    updateScreen();
+}
+
+function getScreenElements() {
+    screen.turn = document.getElementById('turn');
+    screen.moves = document.getElementById('moves');
+    screen.buttons = [];
+    screen.images = [];
+    for (let i = 0; i < 9; i++) {
+        screen.buttons.push(document.getElementById(`button-${i}`));
+        screen.images.push(document.getElementById(`image-${i}`));
+    }
+}
+
+function addGameEvents() {
+    for (let i = 0; i < 9; i++){
+        screen.buttons[i].addEventListener('click', buttonPressListener);
+    }
+}
+
+function buttonPressListener(){
+    const buttonId = event.target.id;
+    const pos = buttonId[buttonId.length - 1];
+
+    gameModel.board[pos] = gameModel.turn;
+    gameModel.toggleTurn();
+    gameModel.moves++;
+
+    updateScreen();
+}
+
+function updateScreen() {
+    // mapping from game model to view 
+    screen.turn.src = imageSource[gameModel.turn];
+    screen.moves.innerHTML = gameModel.moves;
+
+    for (let i = 0; i < 9; i++) {
+        screen.images[i].src = imageSource[gameModel.board[i]];
+        screen.buttons[i].disabled = gameModel.board[i] != marking.U;
+    }
 }
